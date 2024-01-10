58640a7d6553d392824001fbf8efd0ce19e23472
Frank Hu <tianyaoh@hotmail.com>
2024-01-10 04:24:39 +0000

    added rviz_node to attach_to_shelf.launch.py

--- launch/attach_to_shelf.launch.py
+++ launch/attach_to_shelf.launch.py
@@ -1,5 +1,8 @@
+import os
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
@@ -10,46 +13,24 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-
-    package_description = "launch_tests_pkg"
-
     # args that can be set from the command line or a default will be used
     # TextSubstitution(text="0.0") W ill only evaluate that in execution time.
 
     obstacle_arg = DeclareLaunchArgument(
         "obstacle", default_value=TextSubstitution(text="0.3")
     )
+
     degrees_arg = DeclareLaunchArgument(
         "degrees", default_value="-90"
     )
+
     final_approach_arg = DeclareLaunchArgument(
         "final_approach", default_value="True"
     )
-    # rviz_config_file_name_arg = DeclareLaunchArgument(
-    #     "rviz_config_file_name", default_value=TextSubstitution(text="launch_part.rviz")
-    # )
 
     obstacle_f = LaunchConfiguration('obstacle')
     degrees_f = LaunchConfiguration('degrees')
     final_approach_f = LaunchConfiguration('final_approach')
-
-    # rviz_config_file_name_f = LaunchConfiguration('rviz_config_file_name')
-
-    # include another launch file
-    # use items because you need to pass a list with a key-value structure
-    # [(key1,value_x),(key2,value_y),...]
-
-    # start_rviz_launch = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource([
-    #         PathJoinSubstitution([
-    #             FindPackageShare(package_description),
-    #             'launch',
-    #             'start_rviz_with_arguments.launch.py'
-    #         ])
-    #     ]),
-    #     launch_arguments={
-    #         'rviz_config_file_name': rviz_config_file_name_f}.items()
-    # )
 
     approach_load_service_server = Node(
         package='attach_shelf',
@@ -70,6 +51,18 @@ def generate_launch_description():
                    "-final_approach", final_approach_f,
                    ]
     )
+    ############### RVIZ ################
+    # RVIZ Configuration
+    package_description = "attach_shelf"
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'launch_part.rviz')
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
 
     return LaunchDescription([
         obstacle_arg,
@@ -77,5 +70,5 @@ def generate_launch_description():
         final_approach_arg,
         approach_load_service_server,
         pre_approach_v2_node,
-        # rviz_config_file_name_arg,
+        rviz_node
     ])

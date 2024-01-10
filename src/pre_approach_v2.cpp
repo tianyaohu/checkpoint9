e3fbeb17a677fc0fb70910c15f94b62ce368f468
Frank Hu <tianyaoh@hotmail.com>
2024-01-10 19:21:35 +0000

    pre_approach_v2.cpp function structure clean-up

--- src/pre_approach_v2.cpp
+++ src/pre_approach_v2.cpp
@@ -44,8 +44,8 @@ public:
     RCLCPP_INFO(this->get_logger(), "Degrees parameter is: %d", arg_degrees);
 
     this->get_parameter("final_approach", arg_final_approach);
-    RCLCPP_INFO(this->get_logger(), "Degrees parameter is: %d",
-                arg_final_approach);
+    RCLCPP_INFO(this->get_logger(), "Degrees parameter is: %s",
+                arg_final_approach ? "True" : "False");
 
     // Init CallbackGroups
     callback_group_1 = this->create_callback_group(
@@ -84,7 +84,7 @@ public:
 
     // init approach service client
     client_ =
-        this->create_client<attach_shelf::srv::GoToLoading>("attach_shelf");
+        this->create_client<attach_shelf::srv::GoToLoading>("/approach_shelf");
 
     // set initial speed
     speed_linear_x = 0.2;
@@ -94,7 +94,6 @@ public:
   }
 
 private:
-  // attributes
   // call back groups
   rclcpp::CallbackGroup::SharedPtr callback_group_1;
   rclcpp::CallbackGroup::SharedPtr callback_group_2;
@@ -105,10 +104,9 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
-  // approach shelf service client
-  rclcpp::Client<attach_shelf::srv::GoToLoading>::SharedPtr client_;
 
-  // service client parameter
+  // client
+  rclcpp::Client<attach_shelf::srv::GoToLoading>::SharedPtr client_;
   bool attach_load_service_done_ = false;
 
   // speed
@@ -121,104 +119,50 @@ private:
   bool arg_final_approach;
 
   // pre approach params: wall flag;
-  bool wall_reached;
+  bool wall_reached = false;
+  bool finished_rot = false;
 
-  // rot vars
-  bool finished_rot;
-  bool target_inited;
-  double target_yaw;
+  float cur_yaw;
+  float target_yaw;
 
   void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
     // get the laser reading of front
     float front_dist = msg->ranges[540];
 
     if (wall_reached == false) {
-      RCLCPP_INFO(this->get_logger(), "540? %f", front_dist);
+      RCLCPP_INFO(this->get_logger(), "front obstacle distance? %f",
+                  front_dist);
     }
 
     // check if obstacle is within distance, and if wall_reached flag is raised
     if (front_dist <= arg_obstacle && wall_reached == false) {
       speed_linear_x = 0;
       speed_angular_z = 0;
-      // set wall reached flag
+      // init target yaw
+      init_target_yaw();
       wall_reached = true;
     }
   }
 
+  void init_target_yaw() {
+    // get current yaw
+    target_yaw = cur_yaw + (arg_degrees % 360) * M_PI / 180;
+  }
+
   void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
-    // if wall is reached and rotation is not performed
+    // if wall not reached or rotation is finished do nothing
+    const float q_x = msg->pose.pose.orientation.x;
+    const float q_y = msg->pose.pose.orientation.y;
+    const float q_z = msg->pose.pose.orientation.z;
+    const float q_w = msg->pose.pose.orientation.w;
+
+    // Yaw (z-axis rotation)
+    float sinYaw = 2.0f * (q_w * q_z + q_x * q_y);
+    float cosYaw = 1.0f - 2.0f * (q_y * q_y + q_z * q_z);
+    cur_yaw = std::atan2(sinYaw, cosYaw);
+    // if wall is reached and rotation is not done
     if (wall_reached && finished_rot == false) {
-      // if wall not reached or rotation is finished do nothing
-      const float q_x = msg->pose.pose.orientation.x;
-      const float q_y = msg->pose.pose.orientation.y;
-      const float q_z = msg->pose.pose.orientation.z;
-      const float q_w = msg->pose.pose.orientation.w;
-
-      // Yaw (z-axis rotation)
-      float sinYaw = 2.0f * (q_w * q_z + q_x * q_y);
-      float cosYaw = 1.0f - 2.0f * (q_y * q_y + q_z * q_z);
-      float cur_yaw = std::atan2(sinYaw, cosYaw);
-
-      // if target_yaw is not set, set it
-      if (!target_inited) {
-        // set cur_yaw
-        double temp = (arg_degrees % 360) * M_PI / 180;
-        target_yaw = cur_yaw + (arg_degrees % 360) * M_PI / 180;
-        RCLCPP_INFO(this->get_logger(), "target_yaw? %f", target_yaw);
-        RCLCPP_INFO(this->get_logger(), "temp %f", temp);
-
-        if (abs(target_yaw) > M_PI) {
-          target_yaw =
-              target_yaw > 0 ? -2 * M_PI + target_yaw : 2 * M_PI + target_yaw;
-        }
-        // set flag
-        target_inited = true;
-        // if target is inited start turning
-      } else {
-        double turn_delta = cur_yaw - target_yaw;
-        if (abs(turn_delta) > M_PI) {
-          turn_delta =
-              turn_delta > 0 ? -2 * M_PI + turn_delta : 2 * M_PI + turn_delta;
-        }
-        // set angular speed
-        speed_angular_z = min(MAX_ANGULAR_SPEED, turn_delta / -3);
-
-        RCLCPP_INFO(this->get_logger(), "turn_delta? %f", turn_delta);
-        RCLCPP_INFO(this->get_logger(), "cur_yaw? %f", cur_yaw);
-        RCLCPP_INFO(this->get_logger(), "finished_rot? %s",
-                    finished_rot ? "True" : "False");
-
-        if (turn_delta < 0.05) {
-          speed_angular_z = 0;
-          finished_rot = true;
-          RCLCPP_INFO(this->get_logger(), "Final Yaw upon finishing? %f",
-                      cur_yaw);
-
-          // block to wait for service to become available
-          while (!client_->wait_for_service(1s)) {
-            if (!rclcpp::ok()) {
-              RCLCPP_ERROR(this->get_logger(),
-                           "Client interrupted while waiting for service. "
-                           "Terminating...");
-              return;
-            }
-            RCLCPP_INFO(this->get_logger(),
-                        "Service Unavailable. Waiting for Service...");
-          }
-          // service to became available
-
-          // init request
-          auto request =
-              std::make_shared<attach_shelf::srv::GoToLoad::Request>();
-          request->attach_to_shelf = true; // temp need to add one more argument
-          // set service status flag to false;
-          attach_load_service_done_ = false;
-          // send request
-          auto result_future = client_->async_send_request(
-              request, std::bind(&ServiceClient::response_callback, this,
-                                 std::placeholders::_1));
-        }
-      }
+      rotate();
     }
   }
 
@@ -229,12 +173,64 @@ private:
     vel_pub_->publish(message);
   }
 
+  void rotate() {
+    // calc turn delta
+    double turn_delta = cur_yaw - target_yaw;
+    // normalize turn delta
+    if (abs(turn_delta) > M_PI) {
+      turn_delta =
+          turn_delta > 0 ? -2 * M_PI + turn_delta : 2 * M_PI + turn_delta;
+    }
+    // set angular speed
+    speed_angular_z = min(MAX_ANGULAR_SPEED, turn_delta / -2);
+
+    RCLCPP_INFO(this->get_logger(), "turn_delta? %f", turn_delta);
+    RCLCPP_INFO(this->get_logger(), "cur_yaw? %f", cur_yaw);
+    RCLCPP_INFO(this->get_logger(), "finished_rot? %s",
+                finished_rot ? "True" : "False");
+
+    if (turn_delta < 0.05) {
+      speed_angular_z = 0;
+      finished_rot = true;
+      RCLCPP_INFO(this->get_logger(), "Final Yaw upon finishing? %f", cur_yaw);
+      // call approach service
+      call_approach_service();
+    }
+  }
+
+  void call_approach_service() {
+    // block to wait for service to become available
+    while (!client_->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(this->get_logger(),
+                     "Client interrupted while waiting for service. "
+                     "Terminating...");
+        return;
+      }
+      RCLCPP_INFO(this->get_logger(),
+                  "Service Unavailable. Waiting for Service...");
+    }
+    // service to became available
+
+    // init request
+    auto request = std::make_shared<attach_shelf::srv::GoToLoading::Request>();
+    request->attach_to_shelf = true; // temp need to add one more argument
+    // set service status flag to false;
+    attach_load_service_done_ = false;
+    // send request
+    auto result_future = client_->async_send_request(
+        request, std::bind(&PreApproach::response_callback, this,
+                           std::placeholders::_1));
+  }
+
   void response_callback(
-      rclcpp::Client<attach_shelf::srv::GoToLoad>::SharedFuture future) {
+      rclcpp::Client<attach_shelf::srv::GoToLoading>::SharedFuture future) {
     auto status = future.wait_for(1s);
+    // if result ready
     if (status == std::future_status::ready) {
       RCLCPP_INFO(this->get_logger(), "Result: success %s",
-                  future.get()->complete.c_str());
+                  future.get()->complete ? "True" : "False");
+      // raise flag service is done
       attach_load_service_done_ = true;
     } else {
       RCLCPP_INFO(this->get_logger(), "Service In-Progress...");

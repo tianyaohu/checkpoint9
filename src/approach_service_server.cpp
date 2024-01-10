0cbda580d03463d1c004367fb445ac7399ce737b
Frank Hu <tianyaoh@hotmail.com>
2024-01-10 22:26:15 +0000

    scan subscription added to approach server

--- src/approach_service_server.cpp
+++ src/approach_service_server.cpp
@@ -1,10 +1,12 @@
 #include "rclcpp/executors/single_threaded_executor.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 #include <attach_shelf/srv/go_to_loading.hpp>
+#include <cstddef>
 #include <numeric>
-// #include "sensor_msgs/msg/laser_scan.hpp"
 
 #include <memory>
+#include <vector>
 
 using GoToLoading = attach_shelf::srv::GoToLoading;
 using std::placeholders::_1;
@@ -15,14 +17,39 @@ using namespace std;
 class ApproachShelfService : public rclcpp::Node {
 public:
   ApproachShelfService() : Node("approach_shelf_service_server") {
-    srv_ = create_service<GoToLoading>(
-        "approach_shelf",
-        std::bind(&ApproachShelfService::GoToLoading_callback, this, _1, _2));
+
+    // init callbackgroups
+    callback_group_1 = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
+    callback_group_2 = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
+    callback_group_3 = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
+
+    RCLCPP_INFO(this->get_logger(), "After Callbaack group");
+
+    // init subscription options
+    rclcpp::SubscriptionOptions option1;
+    option1.callback_group = callback_group_1;
+
+    // QOS profile
+    rclcpp::QoS qos_profile_subscriber(1);
+    qos_profile_subscriber.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+
+    // init laser subscription with QOS
+    laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "/scan", qos_profile_subscriber,
+        std::bind(&ApproachShelfService::scan_callback, this, _1), option1);
+
+    // transform Broadcaster TODO
+
+    // srv_ = create_service<GoToLoading>(
+    //     "approach_shelf",
+    //     std::bind(&ApproachShelfService::GoToLoading_callback, this, _1, _2),
+    //     rmw_qos_profile_services_default, callback_group_2);
   }
 
 private:
-  rclcpp::Service<GoToLoading>::SharedPtr srv_;
-
   void
   GoToLoading_callback(const std::shared_ptr<GoToLoading::Request> request,
                        const std::shared_ptr<GoToLoading::Response> response) {
@@ -31,11 +58,52 @@ private:
 
     response->complete = false;
   }
-};
+
+
+
+  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+
+
+  // find the ranges with consecutive indices with values greater
+
+  std::cout << "------------------------------------" << endl;
+
+  cout << "length of intensity scan" << msg->intensities.size() << endl;
+
+  // print high intensity ranges
+  // for (size_t i = 0; i < msg->intensities.size(); i++) {
+
+  //   float temp = msg->intensities[i];
+  //   if (temp > 0.001) {
+  //     RCLCPP_INFO(this->get_logger(), "%ldth intensity reading: %f", i,
+  //                 msg->intensities[i]);
+  //   }
+  // }
+
+  // find the high intensity consecutive ranges
+}
+
+//   rclcpp::Service<GoToLoading>::SharedPtr srv_;
+
+rclcpp::CallbackGroup::SharedPtr callback_group_1;
+rclcpp::CallbackGroup::SharedPtr callback_group_2;
+rclcpp::CallbackGroup::SharedPtr callback_group_3;
+
+rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+}
+;
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<ApproachShelfService>());
+  // init node
+  std::shared_ptr<ApproachShelfService> node =
+      std::make_shared<ApproachShelfService>();
+
+  // Initialize one MultiThreadedExecutor object
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);
+  executor.spin();
+
   rclcpp::shutdown();
   return 0;
 }

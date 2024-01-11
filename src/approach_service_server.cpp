a9e0b9278a7f71c28dbb95e41ea59661a94334eb
Frank Hu <tianyaoh@hotmail.com>
2024-01-11 08:30:38 +0000

    transform listener added to approach_service_server

--- src/approach_service_server.cpp
+++ src/approach_service_server.cpp
@@ -1,11 +1,18 @@
+#include "geometry_msgs/msg/pose2_d.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "rclcpp/executors/single_threaded_executor.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/transform_listener.h"
 #include <attach_shelf/srv/go_to_loading.hpp>
-#include <cstddef>
-#include <numeric>
 
+#include <cmath>
+#include <cstddef>
 #include <memory>
+#include <numeric>
+#include <utility>
 #include <vector>
 
 using GoToLoading = attach_shelf::srv::GoToLoading;
@@ -38,31 +45,53 @@ public:
 
     // init laser subscription with QOS
     laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-        "/scan", qos_profile_subscriber,
+        "scan", qos_profile_subscriber,
         std::bind(&ApproachShelfService::scan_callback, this, _1), option1);
 
-    // transform Broadcaster TODO
+    // transform Broadcaster
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
 
-    // srv_ = create_service<GoToLoading>(
-    //     "approach_shelf",
-    //     std::bind(&ApproachShelfService::GoToLoading_callback, this, _1, _2),
-    //     rmw_qos_profile_services_default, callback_group_2);
+    // transform listener from /odom to /laser
+    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    // timer for triggering listener
+    timer_ = this->create_wall_timer(
+        10ms, std::bind(&ApproachShelfService::timer_callback, this),
+        callback_group_2);
+
+    // server
+    srv_ = create_service<GoToLoading>(
+        "approach_shelf",
+        std::bind(&ApproachShelfService::GoToLoading_callback, this, _1, _2),
+        rmw_qos_profile_services_default, callback_group_3);
   }
 
 private:
-  void
-  GoToLoading_callback(const std::shared_ptr<GoToLoading::Request> request,
-                       const std::shared_ptr<GoToLoading::Response> response) {
-    RCLCPP_INFO(this->get_logger(), "attach2shelf request: %s",
-                request->attach_to_shelf ? "True" : "False");
+  void timer_callback() {
+    // init frames needed
+    string target_frame = "robot_front_laser_link";
+    string robot_odom = "robot_odom";
+    // Look up transformation
+    try {
+      t = std::make_shared<geometry_msgs::msg::TransformStamped>(
+          tf_buffer_->lookupTransform(target_frame, robot_odom,
+                                      tf2::TimePointZero));
+    } catch (const tf2::TransformException &ex) {
+      RCLCPP_INFO(this->get_logger(), "Could not transform %s to %s: %s",
+                  target_frame.c_str(), robot_odom.c_str(), ex.what());
+      return;
+    }
 
-    response->complete = false;
+    // Do something with the transform (e.g., print it)
+    RCLCPP_INFO(get_logger(), "Received transform: [%f, %f, %f]",
+                t->transform.translation.x, t->transform.translation.y,
+                t->transform.translation.z);
   }
 
-  std::vector<std::pair<int, int>>
-  getConsecutiveRanges(const std::vector<float> &values) {
+  vector<std::pair<int, int>>
+  getConsecutiveRanges(const vector<float> &values) {
     float THRESH_HOLD = 0.001;
-    std::vector<std::pair<int, int>> ranges;
+    vector<std::pair<int, int>> ranges;
 
     size_t start = 0;
     size_t end = 0;
@@ -89,11 +118,11 @@ private:
     return ranges;
   }
 
-  std::vector<int>
-  findMinIndicesInRanges(const std::vector<float> &values,
-                         const std::vector<std::pair<int, int>> &ranges) {
+  vector<int>
+  findMinIndicesInRanges(const vector<float> &values,
+                         const vector<std::pair<int, int>> &ranges) {
 
-    std::vector<int> minIndices;
+    vector<int> minIndices;
 
     for (const auto &range : ranges) {
       // Find the minimum value and its index within the range
@@ -106,47 +135,101 @@ private:
     return minIndices;
   }
 
+  vector<pair<float, float>> getXYFromIndex(const vector<int> indices,
+                                            const vector<float> dist_values,
+                                            int len_half_range) {
+    vector<pair<float, float>> vec_xy;
+    for (const auto &index : indices) {
+      float rad = static_cast<float>(len_half_range - index) / len_half_range *
+                  M_PI; // here assumes that indices lower than half range
+                        // represent left (negative rads)
+      float dist = dist_values[index];
+
+      cout << "index is " << index << endl;
+      cout << "rad is " << rad << endl;
+      cout << "dist is " << dist << endl;
+
+      // calc x and y
+      float x = sin(rad + M_PI_2) * dist;
+      float y = cos(rad + M_PI_2) * dist;
+
+      cout << "x is " << x << endl;
+      cout << "y is " << y << endl;
+
+      // create pair and store
+      vec_xy.emplace_back(make_pair(x, y));
+    }
+    return vec_xy;
+  }
+
   void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
 
     // find the ranges with consecutive indices with values greater
     vector<pair<int, int>> high_intense_ranges =
         getConsecutiveRanges(msg->intensities);
 
-    // find the indicies with min value
-    vector<int> min_indicies =
+    // find the indices with min value
+    vector<int> min_indices =
         findMinIndicesInRanges(msg->ranges, high_intense_ranges);
 
-    // print to see min indicies
-    for (const auto &i : min_indicies) {
-      RCLCPP_INFO(this->get_logger(),
-                  "min indicies : %d; corresponding value: %f", i,
-                  msg->ranges[i]);
+    // convert min indices into xy vectors
+    vector<pair<float, float>> vec_xy =
+        getXYFromIndex(min_indices, msg->ranges, msg->ranges.size() / 2);
+
+    // check if there is strictly vec_xy has two values
+    if (vec_xy.size() == 2) {
+
+      pair<float, float> vec2mid_pt =
+          make_pair(vec_xy[0].first + vec_xy[1].first,
+                    vec_xy[0].second + vec_xy[1].second);
+
+      cout << "vec2mid_pt first " << vec2mid_pt.first << "; vec2mid_pt second "
+           << vec2mid_pt.second << endl;
     }
 
+    // init transformstamped
+    // geometry_msgs::msg::TransformStamped t;
+    // // corresponding tf variables
+    // t.header.stamp = this->get_clock()->now();
+    // t.header.frame_id = "robot_front_laser_base_link";
+    // t.child_frame_id = "cart_frame";
+
+    // coordinates from the message and set the z coordinate to 0
+    // t.transform.translation.x = cur_pos.x;
+    // t.transform.translation.y = cur_pos.y;
+    // t.transform.translation.z = cur_z;
+
+    // TODO TF BROADCASTER
+
     std::cout << "------------------------------------" << endl;
-
-    // cout << "length of intensity scan" << msg->intensities.size() << endl;
-
-    // print high intensity ranges
-    // for (size_t i = 0; i < msg->intensities.size(); i++) {
-
-    //   float temp = msg->intensities[i];
-    //   if (temp > 0.001) {
-    //     RCLCPP_INFO(this->get_logger(), "%ldth intensity reading: %f", i,
-    //                 msg->intensities[i]);
-    //   }
-    // }
-
-    // find the high intensity consecutive ranges
   }
 
-  //   rclcpp::Service<GoToLoading>::SharedPtr srv_;
+  void
+  GoToLoading_callback(const std::shared_ptr<GoToLoading::Request> request,
+                       const std::shared_ptr<GoToLoading::Response> response) {
+
+    RCLCPP_INFO(this->get_logger(), "attach2shelf request: %s",
+                request->attach_to_shelf ? "True" : "False");
+
+    response->complete = false;
+  }
 
   rclcpp::CallbackGroup::SharedPtr callback_group_1;
   rclcpp::CallbackGroup::SharedPtr callback_group_2;
   rclcpp::CallbackGroup::SharedPtr callback_group_3;
 
+  rclcpp::Service<GoToLoading>::SharedPtr srv_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  // flags
+  bool got_req;
+
+  // transform looked up
+  std::shared_ptr<geometry_msgs::msg::TransformStamped> t;
 };
 
 int main(int argc, char *argv[]) {

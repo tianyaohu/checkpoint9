3be1e01ded3a5e4b35aef4f48444f4b3903d2b7f
Frank Hu <tianyaoh@hotmail.com>
2024-01-12 01:54:26 +0000

    go to shelf achieved; sleep approach

--- src/approach_service_server.cpp
+++ src/approach_service_server.cpp
@@ -1,6 +1,9 @@
+#include "geometry_msgs/msg/detail/twist__struct.hpp"
 #include "geometry_msgs/msg/pose2_d.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/executors/single_threaded_executor.hpp"
+#include "rclcpp/logging.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "tf2_ros/buffer.h"
@@ -14,6 +17,7 @@
 #include <memory>
 #include <mutex>
 #include <numeric>
+#include <thread>
 #include <utility>
 #include <vector>
 
@@ -58,14 +62,14 @@ public:
     // transform listener from /odom to /laser
     tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
-    // timer for triggering listener
-    timer_ = this->create_wall_timer(
-        10ms, std::bind(&ApproachShelfService::timer_callback, this),
-        callback_group_2);
+
+    // init cmd_vel publisher
+    vel_pub_ =
+        this->create_publisher<geometry_msgs::msg::Twist>("/robot/cmd_vel", 10);
 
     // init frames needed COULD BE PARAMETERS
-    target_frame = "robot_front_laser_link";
-    robot_odom = "robot_odom";
+    to_frame = "cart_frame";
+    from_frame = "robot_base_link";
 
     // server
     srv_ = create_service<GoToLoading>(
@@ -75,59 +79,96 @@ public:
   }
 
 private:
-  void broadcastCartTF(const geometry_msgs::msg::TransformStamped &t) {
+  void
+  GoToLoading_callback(const std::shared_ptr<GoToLoading::Request> request,
+                       const std::shared_ptr<GoToLoading::Response> response) {
 
-    // init transformstamped
-    geometry_msgs::msg::TransformStamped tf_togo;
-    // corresponding tf variables
-    tf_togo.header.stamp = t.header.stamp;
-    tf_togo.header.frame_id = "robot_front_laser_base_link";
-    tf_togo.child_frame_id = "cart_frame";
+    RCLCPP_INFO(this->get_logger(), "attach2shelf request: %s",
+                request->attach_to_shelf ? "True" : "False");
 
-    // set translation coordinates
-    vec_mu.lock();
-    tf_togo.transform.translation.x = vec2mid_pt.first;
-    tf_togo.transform.translation.y = vec2mid_pt.second;
+    bool to_shelf = approach_shelf();
 
-    tf_togo.transform.translation.z = t.transform.translation.z;
-    (void)t;
-    vec_mu.unlock();
+    cout << "Within service handler, to shelf success?" << to_shelf << endl;
 
-    tf2::Quaternion q;
-    q.setRPY(0, 0, 0);
-    tf_togo.transform.rotation.x = q.x();
-    tf_togo.transform.rotation.y = q.y();
-    tf_togo.transform.rotation.z = q.z();
-    tf_togo.transform.rotation.w = q.w();
-
-    // TODO TF BROADCASTER
-    tf_broadcaster_->sendTransform(tf_togo);
+    response->complete = to_shelf;
   }
 
-  void timer_callback() {
-
-    // transform looked up
+  bool approach_shelf(float cx = 0) {
+    float THRESH_HOLD = 0.24;
     geometry_msgs::msg::TransformStamped t;
-    // Look up transformation
-    try {
-      t = tf_buffer_->lookupTransform(target_frame, robot_odom,
-                                      tf2::TimePointZero);
-    } catch (const tf2::TransformException &ex) {
-      RCLCPP_INFO(this->get_logger(), "Could not transform %s to %s: %s",
-                  target_frame.c_str(), robot_odom.c_str(), ex.what());
-      return;
-    }
 
-    // check if both legs of cart are detected
-    if (got_both_legs) {
-      RCLCPP_INFO(this->get_logger(), "Trying to Broadcast TF");
-      broadcastCartTF(t);
-    }
+    while (rclcpp::ok()) {
+      // Look up transformation
+      try {
+        t = tf_buffer_->lookupTransform(from_frame, to_frame,
+                                        tf2::TimePointZero);
+      } catch (const tf2::TransformException &ex) {
+        RCLCPP_ERROR(this->get_logger(), "Could not transform %s to %s: %s",
+                     to_frame.c_str(), from_frame.c_str(), ex.what());
+        return false;
+      }
 
-    // Do something with the transform (e.g., print it)
-    RCLCPP_INFO(get_logger(), "Received transform: [%f, %f, %f]",
-                t.transform.translation.x, t.transform.translation.y,
-                t.transform.translation.z);
+      // calculate distance error
+      float error_dist = sqrt(pow(t.transform.translation.x + cx, 2) +
+                              pow(t.transform.translation.y, 2) +
+                              pow(t.transform.translation.z, 2));
+
+      cout << "error distance is " << error_dist << endl;
+
+      // init kp
+      float kp_distance = 0.4;
+      float kp_yaw = 1;
+
+      // calculate true delta
+      float error_yaw;
+      if (error_dist < THRESH_HOLD * 2) {
+        // set linear x to 0
+        kp_distance /= 2;
+
+        // init quaternion
+        tf2::Quaternion q(t.transform.rotation.x, t.transform.rotation.y,
+                          t.transform.rotation.z, t.transform.rotation.w);
+        tf2::Matrix3x3 m(q);
+        double roll, pitch, error_yaw;
+        m.getRPY(roll, pitch, error_yaw);
+
+        cout << "close enoough, error yaw is " << error_yaw << endl;
+      } else {
+        error_yaw = atan2(t.transform.translation.y, t.transform.translation.x);
+        cout << "far , error yaw is " << error_yaw << endl;
+      }
+
+      // raw speed
+      float raw_linear_x = kp_distance * error_dist;
+      float raw_angular_z = kp_yaw * error_yaw;
+
+      // move
+      move(raw_linear_x, raw_angular_z);
+
+      // close enought to target frame??
+      if (abs(error_yaw) < THRESH_HOLD && error_dist < THRESH_HOLD) {
+        // stop
+        move(0.2, 0);
+        cout << "going underneath shelf" << endl;
+        this_thread::sleep_for(3s);
+        move(0, 0);
+        cout << "stopping underneath shelf???" << endl;
+
+        return true;
+      }
+      // sleep
+      this_thread::sleep_for(50ms);
+    }
+    // this should never be reached
+    return false;
+  }
+
+  void move(float linear_x, float angular_z, float MAX_LINEAR_X = 0.5,
+            float MAX_ANGULAR_Z = 0.5) {
+    geometry_msgs::msg::Twist msg;
+    msg.linear.x = clamp(linear_x, -MAX_LINEAR_X, MAX_LINEAR_X);
+    msg.angular.z = clamp(angular_z, -MAX_ANGULAR_Z, MAX_ANGULAR_Z);
+    vel_pub_->publish(msg);
   }
 
   vector<std::pair<int, int>>
@@ -160,21 +201,15 @@ private:
     return ranges;
   }
 
-  vector<int>
-  findMinIndicesInRanges(const vector<float> &values,
-                         const vector<std::pair<int, int>> &ranges) {
+  vector<int> getMidRangeIndex(const vector<std::pair<int, int>> &ranges) {
 
-    vector<int> minIndices;
+    vector<int> mid_range_indices;
 
     for (const auto &range : ranges) {
-      // Find the minimum value and its index within the range
-      auto iterator = min_element(values.begin() + range.first,
-                                  values.begin() + range.second);
-      // Calculate the index of the maximum element
-      int index = distance(values.begin(), iterator);
-      minIndices.emplace_back(index);
+      int mid_range = (range.first + range.second) / 2;
+      mid_range_indices.emplace_back(mid_range);
     }
-    return minIndices;
+    return mid_range_indices;
   }
 
   vector<pair<float, float>> getXYFromIndex(const vector<int> indices,
@@ -183,26 +218,14 @@ private:
                                             float angle_increment) {
     vector<pair<float, float>> vec_xy;
     for (const int &index : indices) {
-      //   float rad = static_cast<float>(len_half_range - index) /
-      //   len_half_range *
-      //               M_PI; // here assumes that indices lower than half range
-      //                     // represent left (negative rads)
+
+      // calc angle and distance with index
       float rad = angle_start + angle_increment * index;
       float dist = dist_values[index];
 
-      cout << "index is " << index << endl;
-      cout << "rad is " << rad << endl;
-      cout << "dist is " << dist << endl;
-
-      // calc x and y
-      //   float x = sin(rad + M_PI_2) * dist;
-      //   float y = cos(rad + M_PI_2) * dist;
-
-      float x = sin(rad + M_PI_2) * dist;
-      float y = cos(rad + M_PI_2) * dist;
-
-      cout << "x is " << x << endl;
-      cout << "y is " << y << endl;
+      // calculate x and y
+      float x = cos(rad) * dist;
+      float y = sin(rad) * dist;
 
       // create pair and store
       vec_xy.emplace_back(make_pair(x, y));
@@ -225,21 +248,45 @@ private:
     vec_mu.unlock();
   }
 
+  void broadcastCartTF(const sensor_msgs::msg::LaserScan::SharedPtr scan_msg) {
+    // init transformstamped
+    geometry_msgs::msg::TransformStamped tf_togo;
+    // corresponding tf variables
+    tf_togo.header.stamp = scan_msg->header.stamp;
+    tf_togo.header.frame_id = "robot_front_laser_base_link";
+    tf_togo.child_frame_id = "cart_frame";
+
+    // set translation coordinates
+    vec_mu.lock();
+    tf_togo.transform.translation.x = vec2mid_pt.first;
+    tf_togo.transform.translation.y = vec2mid_pt.second;
+    tf_togo.transform.translation.z = 0; // asssuming perfectly flat ground
+    vec_mu.unlock();
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0);
+    tf_togo.transform.rotation.x = q.x();
+    tf_togo.transform.rotation.y = q.y();
+    tf_togo.transform.rotation.z = q.z();
+    tf_togo.transform.rotation.w = q.w();
+
+    // TODO TF BROADCASTER
+    tf_broadcaster_->sendTransform(tf_togo);
+  }
+
   void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
 
     // find the ranges with consecutive indices with values greater
     vector<pair<int, int>> high_intense_ranges =
         getConsecutiveRanges(msg->intensities);
 
-    // Count the number of legs detected (one xy pairs is assumed to be one
-    // legg)
+    // Check cart leg count
     if (high_intense_ranges.size() == 2) {
       // set flag for tf read
       got_both_legs = true;
 
       // find the indices with min value
-      vector<int> min_indices =
-          findMinIndicesInRanges(msg->ranges, high_intense_ranges);
+      vector<int> min_indices = getMidRangeIndex(high_intense_ranges);
 
       // convert min indices into xy vectors
       vector<pair<float, float>> vec_xy = getXYFromIndex(
@@ -253,17 +300,11 @@ private:
       // check if there are
       cout << "MUST have exactly two legs to do broadcast" << endl;
     }
-    std::cout << "------------------------------------" << endl;
-  }
 
-  void
-  GoToLoading_callback(const std::shared_ptr<GoToLoading::Request> request,
-                       const std::shared_ptr<GoToLoading::Response> response) {
-
-    RCLCPP_INFO(this->get_logger(), "attach2shelf request: %s",
-                request->attach_to_shelf ? "True" : "False");
-
-    response->complete = false;
+    // BroadCast TF if both legs of cart are detected
+    if (got_both_legs) {
+      broadcastCartTF(msg);
+    }
   }
 
   rclcpp::CallbackGroup::SharedPtr callback_group_1;
@@ -271,15 +312,15 @@ private:
   rclcpp::CallbackGroup::SharedPtr callback_group_3;
 
   rclcpp::Service<GoToLoading>::SharedPtr srv_;
-  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
   std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
   std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-  rclcpp::TimerBase::SharedPtr timer_;
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
 
   // frames
-  string target_frame;
-  string robot_odom;
+  string to_frame;
+  string from_frame;
 
   // vec2 mid_pt;
   std::pair<float, float> vec2mid_pt;
